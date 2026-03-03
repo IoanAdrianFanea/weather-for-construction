a8acc2c07003087dd75fd8052dd1dfa05ad829e4
IoanAdrianFanea <ec24483@qmul.ac.uk>
2026-03-03 20:16:27 +0000

    chore: project scaffolding (Vite + React + Tailwind)

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="min-h-screen flex items-center justify-center text-4xl font-bold">
+      Tailwind works ✅
+    </div>
     </>
   )
 }
